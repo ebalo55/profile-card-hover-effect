3f6cdfc5afbf5524520b2c5120502cf8e58971b9
ebalo <tuema55@gmail.com>
2020-05-26 21:57:29 +0200

    Dist commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <profile-card :name="profile1.name" :pic-url="profile1.picUrl" :alt-text="profile1.altText"></profile-card>
+        <profile-card :name="profile2.name" :pic-url="profile2.picUrl" :alt-text="profile2.altText"></profile-card>
+        <profile-card :name="profile3.name" :pic-url="profile3.picUrl" :alt-text="profile3.altText"></profile-card>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfileCard from "@/components/profileCard";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: { ProfileCard },
+    data: function() {
+        return {
+            profile1: {
+                name: "Susan Cox",
+                picUrl: "assets/portrait1.jpg",
+                altText: "Susan Cox's profile"
+            },
+            profile2: {
+                name: "John Calcote",
+                picUrl: "assets/portrait2.jpg",
+                altText: "John Calcote's profile"
+            },
+            profile3: {
+                name: "Charlotte Clinton",
+                picUrl: "assets/portrait3.jpg",
+                altText: "Charlotte Clinton's profile"
+            },
+        };
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/profileCard.vue
+++ src/components/profileCard.vue
@@ -1,13 +1,23 @@
 <template>
-    $END$
+    <div class="box">
+        <img :src="picUrl" :alt="altText">
+        <p>{{ name }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "profileCard"
+        name: "profileCard",
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            picUrl: {
+                type: String,
+                required: true,
+            },
+            altText: String,
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
